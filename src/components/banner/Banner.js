9c019611b496aa10701135f6cc1a91a700b22daa
Marty Siksnis <siksnis@Martys-MacBook-Pro.local>
2020-09-23 12:38:00 +0200

    Started building a SearchDates component where you will be able to pick the dates and search for vacancies.

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@material-ui/core";
+import SearchDates from "../searchDates/SearchDates";
 
 function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <div className="banner">
+      <div className="banner__search">
+        {showSearch && <SearchDates />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          className="banner__searchButton"
+          variant="outlined"
+        >
+          Search dates
+          {/* {showSearch ? "Hide" : "Search Dates"} */}
+        </Button>
+      </div>
       <div className="banner__info">
         <h1>Get out and stretch your imagination</h1>
         <h5>
---
+++ src/components/searchDates/SearchDates.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SearchDates.css";
+
+function SearchDates() {
+  return <div className="searchDates">When?</div>;
+}
+
+export default SearchDates;
