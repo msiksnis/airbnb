2da45454fd874726ed18dd6a97bac87f0b625d9c
Marty Siksnis <siksnis@Martys-MacBook-Pro.local>
2020-09-27 23:52:30 +0200

    Installed and started using react-router-dom for routing to different pages, Started working on SearchPage component.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import SearchPage from "./components/searchPage/SearchPage";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
-
-      {/* Header */}
-      {/* Banner */}
-      {/* Cards */}
-      {/* Footer */}
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
