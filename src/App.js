2b1a7b81e475188b74a21ea9f2d3892770e4bbf6
Marty Siksnis <siksnis@Martys-MacBook-Pro.local>
2020-09-27 08:32:39 +0200

    Finished footer for now. Later gonna add more to it. Also deployed app to fireabse.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Home from "./components/home/Home";
 import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Home />
+      <Footer />
 
       {/* Header */}
       {/* Banner */}
